b0bc1c8be1396f87ace032cd30faf15adb5992e1
ajquirk <ajquirk@us.ibm.com>
2022-02-01 19:17:55 -0500

    remove branding from PR

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'NFT 4 Cause Discord Verification',
+    title: 'authorization-bot',
     htmlAttrs: {
       lang: 'en'
     },
