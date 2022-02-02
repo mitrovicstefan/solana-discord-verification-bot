b1c8afec23d5021e910337f4cea955f2250d50af
ajquirk <ajquirk@us.ibm.com>
2022-02-02 18:07:14 -0500

    configurable project name

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'authorization-bot',
+    title: process.env.PROJECT_NAME || 'authorization-bot',
     htmlAttrs: {
       lang: 'en'
     },
